02ca94fc0d419a4262f7d70580e0b8409d1bc1c0
Yiyi Sun <yiyisun@gmail.com>
2025-07-14 19:31:49 -0400

    update API endpoint to '/api' and add README file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
-app.get('/', cors(), async (req, res) => {  
+app.get('/api', cors(), async (req, res) => {  
   let response = await fetch('https://xkcd.com/info.0.json');
   const current = await response.json();
   const num = Math.floor(Math.random() * current.num) + 1;
