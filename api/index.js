0dd23e52f018df7d358103a18540c1719e86604b
Yiyi Sun <yiyisun@gmail.com>
2025-07-14 20:39:12 -0400

    Implement Express server and CORS support; add Vercel configuration for API routing

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+
+app.get('/', cors(), async (req, res) => {
+  let response = await fetch('https://xkcd.com/info.0.json');
+  const current = await response.json();
+  const num = Math.floor(Math.random() * current.num) + 1;
+  response = await fetch(`https://xkcd.com/${num}/info.0.json`);
+  const comic = await response.json();
+  res.json(comic);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
+module.exports = app; 
